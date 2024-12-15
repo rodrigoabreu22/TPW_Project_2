7f0a7c3ef74a5e73e277ab23d7fa8b3dc4071dfc
João Neto <jneto04@ua.pt>
2024-12-15 22:13:19 +0000

    navbar acabada só falta a offer.count - Ricardo

--- AmorCamisola_frontend/src/app/navbar-login/navbar-login.component.ts
+++ AmorCamisola_frontend/src/app/navbar-login/navbar-login.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-login',
-  imports: [],
+  imports: [CommonModule,RouterModule,RouterLink],
   templateUrl: './navbar-login.component.html',
   styleUrl: './navbar-login.component.css'
 })
 export class NavbarLoginComponent {
+  @Input() username: string = "";
 
+  constructor() { }
 }
