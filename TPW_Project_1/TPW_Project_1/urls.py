a32b66fb3a4889c0c870170fee6be773bc9d08d0
João Neto <jneto04@ua.pt>
2024-12-16 16:57:17 +0000

    merge random da main toda destruida

--- TPW_Project_1/TPW_Project_1/urls.py
+++ TPW_Project_1/TPW_Project_1/urls.py
@@ -63,7 +63,6 @@ urlpatterns = [
     path('ws/offers/<int:id>/', views.get_offer_by_id, name='get_offer_by_id'),
     path('ws/users/<int:user_id>/favorites/', views.user_favorites, name='user_favorites'),  
     path('ws/users/<int:user_id>/favorites/<int:product_id>/', views.user_favorites, name='user_favorites_product'), 
-    path('ws/users/<int:id>', views.get_userProfile_by_id , name='get_userProfile_by_id'),
     path('ws/product-reports/', views.get_products_reports , name='get_products_reports'),
     path('ws/user-reports/', views.get_user_reports , name='get_user_reports'),   
     path('ws/users/<int:id>', views.userProfile_by_id , name='userProfile_by_id') 
